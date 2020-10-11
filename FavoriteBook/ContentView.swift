31908ee47e1385724c32685a5960e7814d55db25
aatakanakman <48566734+aatakanakman@users.noreply.github.com>
2020-10-12 00:40:25 +0300

    Initial Commit

---
+++ FavoriteBook/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FavoriteBook
+//
+//  Created by Ali Atakan AKMAN on 12.10.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
